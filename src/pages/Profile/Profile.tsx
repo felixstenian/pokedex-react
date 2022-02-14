9f10b0710d44d2b274e8d6cc3b4880f8632f4d46
felixstenian <felixstenian@gmail.com>
2022-02-14 20:22:50 -0300

    create page profile pokemon

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,9 +1,59 @@
-// import { Link, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+import './styles.scss'
 
 const Profile = () => {
-  // const { id } = useParams()
+  const { name }: any = useParams()
 
-  return <div>Profile</div>
+  const [pokemonData, setPokemonData] = useState<any>()
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      const data = await response.json()
+      setPokemonData(data)
+    }
+
+    getData()
+  }, [name])
+
+  console.log({ pokemonData })
+
+  return (
+    <div className='container'>
+      <main>
+        <header>
+          <h1>{pokemonData?.name}</h1>
+          <h1>#{pokemonData?.order}</h1>
+        </header>
+        <div>
+          <ul>
+            {pokemonData?.types?.map((type: any) => (
+              <li>{type.type.name}</li>
+            ))}
+          </ul>
+        </div>
+        <section>
+          <div className='fotos'>
+            <img src={pokemonData?.sprites.front_default} alt={pokemonData?.name} />
+          </div>
+          <div className='starts'>
+            <ul>
+              {pokemonData?.stats?.map((stat: any) => (
+                <li>
+                  {stat?.stat?.name} - {stat?.base_stat}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </main>
+      <p>
+        <Link to='/'>Voltar</Link>
+      </p>
+    </div>
+  )
 }
 
 export default Profile
